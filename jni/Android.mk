daf26e817ff5cfb2e804f2cf231bc771307d9123
Jongyeol Choi <jong10@gmail.com>
2011-11-23 00:22:42 +0900

    Add cscope, libncurses, regex
    1. cscope from http://cscope.sf.net
    2. libncurses from http://code.google.com/p/android-cruft/downloads/list
    3. regex.c regex.h from http://www.clearsilver.net
    
    Signed-off-by: Jongyeol Choi <jong10@gmail.com>

---
+++ jni/Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES	:= \
+	fscanner.c \
+	egrep.c \
+	alloc.c \
+	basename.c \
+	build.c \
+	command.c \
+	compath.c \
+	crossref.c \
+	dir.c \
+	display.c \
+	edit.c \
+	exec.c \
+	find.c \
+	help.c \
+	history.c \
+	input.c \
+	invlib.c \
+	logdir.c \
+	lookup.c \
+	main.c \
+	mouse.c \
+	mygetenv.c \
+	mypopen.c \
+	vpaccess.c \
+	vpfopen.c \
+	vpinit.c \
+	vpopen.c \
+	regex.c
+
+LOCAL_MODULE	:= cscope
+LOCAL_CFLAGS	:= -DHAVE_CONFIG_H -lncurses
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/. $(LOCAL_PATH)/../include
+
+include $(BUILD_EXECUTABLE)
